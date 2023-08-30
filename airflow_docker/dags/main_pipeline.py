bccb1ab3c0e650744df29be4d89256322931d9fa
Shahin Yusifli <sahinyusifli98@gmail.com>
2023-08-30 17:21:28 +0300

    Add file check step and context manager

--- airflow_docker/dags/main_pipeline.py
+++ airflow_docker/dags/main_pipeline.py
@@ -3,9 +3,10 @@ import psycopg2
 from airflow import DAG
 from airflow.hooks.base_hook import BaseHook
 from airflow.operators.python_operator import PythonOperator
-from airflow.sensors.sql_sensor import SqlSensor
-from airflow.utils.dates import days_ago
-from datetime import datetime, timedelta
+from airflow.sensors.python import PythonSensor
+import datetime
+from datetime import timedelta
+import os
 
 # PostgreSQL Connection
 POSTGRES_CONN_ID = 'postgres_localhost'
@@ -13,36 +14,27 @@ POSTGRES_CONN_ID = 'postgres_localhost'
 default_args = {
     'owner': 'airflow',
     'depends_on_past': False,
-    'start_date': days_ago(1),
+    'start_date': datetime(2023, 8, 15),
     'schedule_interval': '@daily',
     'retries': 1,
-    'retry_delay': timedelta(minutes=5),
 }
+csv_filepath = '/opt/airflow/dags/Netflix_dataset.csv'
 
-dag = DAG(
-    'import_csv_to_postgres',
-    default_args=default_args,
-    schedule_interval=None,  # Set your preferred schedule
-    catchup=False,
-)
+def check_file_exist(file_path):
+    return os.path.isfile(file_path)
 
-# Python function to load CSV and insert/upsert data into PostgreSQL
 def insert_upsert_csv_data_to_postgres():
-    # Load CSV data using Pandas
-    csv_filepath = '/opt/airflow/dags/Netflix_dataset.csv'
     df = pd.read_csv(csv_filepath, sep=';')
-
-    # Get PostgreSQL connection parameters from Airflow connection
     conn = BaseHook.get_connection(POSTGRES_CONN_ID)
     conn_params = {
-        'dbname': conn.schema,
-        'user': conn.login,
-        'password': conn.password,
-        'host': conn.host,
-        'port': conn.port
-    }
+            'dbname': conn.schema,
+            'user': conn.login,
+            'password': conn.password,
+            'host': conn.host,
+            'port': conn.port
+        }
 
-    # Insert or upsert data into PostgreSQL using INSERT ON CONFLICT
+    
     conn = psycopg2.connect(**conn_params)
     cursor = conn.cursor()
     for index, row in df.iterrows():
@@ -74,23 +66,23 @@ def insert_upsert_csv_data_to_postgres():
     cursor.close()
     conn.close()
 
-# Create an upsert task to execute the Python function
-upsert_task = PythonOperator(
-    task_id='upsert_csv_to_postgres_task',
-    python_callable=insert_upsert_csv_data_to_postgres,
-    dag=dag,
-)
+with DAG(
+    'import_csv_to_postgres',
+    default_args=default_args,
+    catchup=False ):
 
-success_sensor_task = SqlSensor(
-    task_id='success_main_etl_task',
-    conn_id=POSTGRES_CONN_ID,
-    sql="SELECT 1",
-    mode="poke",
-    timeout=600,  # Adjust as needed
-    poke_interval=30,  # Adjust as needed
-    retries=5,  # Adjust as needed
-    dag=dag,
-)
+    upsert_task = PythonOperator(
+        task_id='upsert_csv_to_postgres_task',
+        python_callable=insert_upsert_csv_data_to_postgres
+    )
 
-# Set task dependencies
-upsert_task >> success_sensor_task
+    check_file_exist_in_path = PythonSensor(
+        task_id='check_file_exist_in_path',
+        python_callable=check_file_exist,
+        op_args=[csv_filepath],
+        mode='poke',
+        timeout=600
+    )   
+
+    
+    check_file_exist_in_path >> upsert_task
